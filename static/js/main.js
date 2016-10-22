e22b82855d1548b54c090269815f948c7ebac949
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-10-22 09:30:04 -0700

    Make sure cells are placed IN rows

--- static/js/main.js
+++ static/js/main.js
@@ -8,14 +8,30 @@ $(document).ready(function(){
     // Make the grid
     var tile_size = $('#main-container').width()/4;
     for(i = 0; i < 4; i++){
-        $('#main-container').append('<tr>');
+        $('#main-container').append('<tr id="row-' + i + '"></tr>');
         for(j = 0; j < 4; j++){
             var tile = "<td class='tile' id='tile-"+ i + "-" + j +"'></td>";
-            $('#main-container').append(tile);
+            var row = "#row-" + i;
+            $(row).append(tile);
             $('.tile').css({'width': tile_size, 'height': tile_size});
         }
-        $('#main-container').append('</tr>');
     }
 
+    // Attempt to add a tile.
+    // Return true on success
+    // Return false on failure.
+    function addTile(){
 
+    }
+
+    // Returns a random grid position
+    // where a tile may be placed.
+    function randomPosition(){
+
+    }
+
+    // Returns true if a grid space is empty
+    function isEmpty(){
+
+    }
 });
