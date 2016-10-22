7f152f61075469f891a923b2b00a1abb84c938e7
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-10-22 09:49:21 -0700

    Add lots of empty helper functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    // A grid to keep track of occuppied positions.
+    var GRID = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+
     // Place the grid container
     var middle = $(window).width()/2;
     var push = middle - $('#main-container').width()/2;
@@ -17,6 +25,27 @@ $(document).ready(function(){
         }
     }
 
+    // Gets a tile object based on the given
+    // position.
+    // returns false if no tile is available.
+    function getTileFromPosition(position){
+
+    }
+
+    // Moves all tiles in a specified direction
+    // returns false if all tiles are unable to move
+    // in the specified direction.
+    function move(){
+
+    }
+
+    // Moves the tile at the given position
+    // one space in the given direction.
+    // returns false if unable to move.
+    function moveTile(position, direction){
+
+    }
+
     // Attempt to add a tile.
     // Return true on success
     // Return false on failure.
@@ -27,11 +56,31 @@ $(document).ready(function(){
     // Returns a random grid position
     // where a tile may be placed.
     function randomPosition(){
-
+        var x = Math.random() * 3;
+        var y = Math.random() * 3;
+        return [x, y];
     }
 
     // Returns true if a grid space is empty
-    function isEmpty(){
+    function isEmpty(coords){
+        var row = coords[0];
+        var col = coords[1];
+        if(GRID[row][col] == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    // Returns true if all tiles are occuppied
+    function gameOver(){
+        for(i = 0; i < 4; i++){
+            for(j = 0; j < 4; j++){
+                if(GRID[i][j] == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 });
